cbf3e3c1de3683fd6c894f966bfd35e6805e954b
Rafael Avarado <fania4life@gmail.com>
2019-10-23 02:37:44 -0400

    cleanup

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import 'react-app-polyfill/ie11';       // For IE 11 Support!
-
-import React from "react"
-import ReactDOM from "react-dom"
-
-import App from "./app/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
